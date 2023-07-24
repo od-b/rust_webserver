c836ca74c7c834b972a0170ee784f3511b2faf21
od-b <od.bjerke@gmail.com>
2023-07-24 17:43:23 +0200

    refreshing

---
+++ notes/notes.sh
@@ -0,0 +1,8 @@
+# run test, include prints
+# cargo test -- --nocapture
+cargo test -- --show-output
+
+cargo test [inclusive_filter] -- --show-output
+            # test-name
+            # mod-name
+
